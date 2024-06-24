0a858063639a613c1eea61912075e2ada4a49630
Bo <bocalpoly@gmail.com>
2024-06-24 00:18:55 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the path to panel.conf
-PANEL_CONF="/home/sparky/.config/lxqt/panel.conf" # Replace with the actual path to panel.conf
+PANEL_CONF="/path/to/your/panel.conf"  # Replace with the actual path to panel.conf
 
 # Define the path to SparkyBotMini.desktop
 SPARKYBOT_DESKTOP="/home/sparky/SparkyBotMini.desktop"
@@ -16,8 +16,10 @@ fi
 SIZE=$(grep -oP 'apps\\[0-9]+' "$PANEL_CONF" | wc -l)
 NEXT_INDEX=$((SIZE + 1))
 
-# Add SparkyBotMini.desktop to panel.conf
+# Add SparkyBotMini.desktop after the existing apps in panel.conf
 echo "apps\\$NEXT_INDEX\\desktop=$SPARKYBOT_DESKTOP" >> "$PANEL_CONF"
+
+# Update apps\size to reflect the new total number of apps
 echo "apps\\size=$((SIZE + 1))" >> "$PANEL_CONF"
 
-echo "App added to panel."
+echo "App added to panel.conf."
