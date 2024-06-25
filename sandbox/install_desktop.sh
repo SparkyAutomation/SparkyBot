b8866f03b8826310501e1f5607dd6b535c85dd32
Bo <bocalpoly@gmail.com>
2024-06-25 09:21:45 -0700

    Add files via upload

---
+++ sandbox/install_desktop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+################ Phase 2: Install desktop environment ################
+
+
+echo "##################################################################"
+echo "########################## Phase 2 done ##########################"
+sleep 2
+
+
+echo "##################################################################"
+echo "########################## Phase 3 done ##########################"
+sleep 2
+
+script2_path="$HOME/install_desktop.sh"
+script3_path="$HOME/config_desktop.sh"
+
+# Add script3 to crontab and remove script2
+(crontab -l; echo "@reboot $script3_path") | crontab -
+crontab -l | grep -v "@reboot $script2_path" | crontab -
+
+sudo reboot
---
+++ sandbox/update_upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+################ Phase 1: Update and upgrade ################
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo "##################################################################"
+echo "########################## Phase 1 done ##########################"
+sleep 2
+
+script1_path="$HOME/update_upgrade.sh"
+script2_path="$HOME/install_desktop.sh"
+
+# Add script2 to crontab and remove script1
+(crontab -l; echo "@reboot $script2_path") | crontab -
+crontab -l | grep -v "@reboot $script1_path" | crontab -
+
+sudo reboot
