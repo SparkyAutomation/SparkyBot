cba159059d6a3dec3a06c29e8f073df26ae8078c
Bo <bocalpoly@gmail.com>
2024-06-25 10:08:37 -0700

    Update config_desktop.sh

--- sandbox/config_desktop.sh
+++ sandbox/config_desktop.sh
@@ -1,12 +1,12 @@
 ################ Phase 4: Configure the desktop for LXQt ################
 
 echo "##################################################################"
-echo "########################## Phase 4 done ##########################"
+echo "########################## Phase 3 done ##########################"
 sleep 2
 
 script3_path="$HOME/config_desktop.sh"
 
 # Remove script3 from crontab
-crontab -l | grep -v "@reboot $script3_path" | crontab -
+(crontab -l | grep -v "@reboot $script3_path") | crontab -
 
-sudo reboot
+echo "All phases completed."
